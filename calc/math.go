d1c9dd2695d39708a16e7ad5c62e83fbd04f1d43
Simeon Korchev <simeon.korchev@sap.com>
2020-06-26 13:58:00 +0300

    Add check for operands count

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,9 @@
 package calc
-
-func Add(operands ...int) int {
+import "errors"
+func Add(operands ...int) (int, error) {
+	if len(operands) < 2 {
+		return 0, errors.New("provide more than  1 numbers")
+	}
 	sum := 0
 	for _, operand := range operands {
 		sum += operand
